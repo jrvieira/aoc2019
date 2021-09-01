a8b9cc3af90a7824d6d937302fbfc0cc02053d4a
jrvieira <github@jrvieira.com>
2021-09-01 01:30:17 +0000

    day 14 part 1

--- 14.hs
+++ 14.hs
@@ -1,14 +1,44 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 import Zero.Zero
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.List.Split
+import Data.Foldable
+import Data.Maybe
+import Data.Map as M
+import Control.Arrow
+
+parse :: String -> M.Map Ch (Int,[(Ch,Int)])
+parse s = M.fromList ls
+   where
+   ls = fmap parsel $ splitOn "\n" s
+   parsel :: String -> (Ch,(Int,[(Ch,Int)]))
+   parsel s = (ch,(n,is')) -- # show (ch,n)
+      where
+      [is,p] = splitOn "=>" s  # s
+      (ch,n) = parse' p
+      is' = fmap parse' $ splitOn ", " is
+      parse' s = let ~[a,b] = words s in (b,read a)
+
+test :: IO ()
+test = do
+   input <- fmap parse . init . splitOn "\n\n" <$> readFile "14.test"
+   teqt "part 1" [31,165,13312,180697,2210736] $ fmap (flip η "ORE") input
 
 main :: IO ()
 main = do
-   print "ok"
-   print solution
+   input <- parse . init <$> readFile "14.txt"
+   print $ η input "ORE"
 
-solution :: [Integer]
-solution = (2^) <$> [0..63]
+type Ch = String
+
+(><) :: Int -> Int -> Int
+_ >< 0 = 1
+p >< n = succ $ div (pred n) p
+
+η :: M.Map Ch (Int,[(Ch,Int)]) -> Ch -> Int
+η m ch = prod >< (sum $ fmap (uncurry (*) . (η m . fst &&& snd)) is) -- # show (prod,is)
+   where
+   is :: [(Ch,Int)]
+   is = M.toList $ M.map fromJust $ M.filter isJust $ M.map (fmap snd . find ((== ch) . fst) . snd) $ m
+   prod = maybe 1 fst $ m M.!? ch
 
